2b17a05f55545410fc41ecb7e311d3b6a1c6902c
Taras <partarstu@gmail.com>
2025-07-11 17:41:12 +0200

    add

---
+++ src/main/java/org/tarik/ta/dto/TestCaseExecutionPlan.java
@@ -0,0 +1,14 @@
+package org.tarik.ta.dto;
+
+import org.tarik.ta.annotations.JsonClassDescription;
+import org.tarik.ta.annotations.JsonFieldDescription;
+
+
+import java.util.List;
+
+@JsonClassDescription("the list of identified by you tool requests")
+public record TestCaseExecutionPlan(
+        @JsonFieldDescription("contains all identified by test step execution plans")
+        List<TestStepExecutionPlan> testStepExecutionPlans
+) {
+}
